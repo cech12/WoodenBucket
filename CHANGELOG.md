1423c1918cc222b748c237376deebe093514e076
Christian Voß <cech12@gmail.com>
2022-03-03 20:49:49 +0100

    update to Forge 1.18.2-40.0.2 & BucketLib 1.18.2-0.5.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Forge Recommended Versioning](https://mcforge.readthedocs.io/en/latest/conventions/versioning/).
 
+## [1.18.2-1.0.1.0] - 2022-03-03
+### Changed
+- Update to Forge 1.18.2-40.0.2
+- Update BucketLib mod to 1.18.2-0.5.1.0
+
 ## [1.18.1-1.0.0.0] - 2022-03-02
 ### Added
 - Wooden Bucket with durability of 25 and burning & freezing effect
--- src/main/java/cech12/woodenbucket/init/ModTags.java
+++ src/main/java/cech12/woodenbucket/init/ModTags.java
@@ -1,13 +1,11 @@
 package cech12.woodenbucket.init;
 
 import cech12.woodenbucket.WoodenBucketMod;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.EntityTypeTags;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Fluid;
-import net.minecraft.tags.FluidTags;
-import net.minecraft.tags.Tag;
 
 import javax.annotation.Nonnull;
 
@@ -15,23 +13,23 @@ public class ModTags {
 
     public static class Fluids {
 
-        public static final Tag.Named<Fluid> CRACKING = tag("cracking");
-        public static final Tag.Named<Fluid> BURNING = tag("burning");
-        public static final Tag.Named<Fluid> FREEZING = tag("freezing");
+        public static final TagKey<Fluid> CRACKING = tag("cracking");
+        public static final TagKey<Fluid> BURNING = tag("burning");
+        public static final TagKey<Fluid> FREEZING = tag("freezing");
 
-        private static Tag.Named<Fluid> tag(@Nonnull String name) {
-            return FluidTags.bind(WoodenBucketMod.MOD_ID + ":" + name);
+        private static TagKey<Fluid> tag(@Nonnull String name) {
+            return TagKey.create(Registry.FLUID_REGISTRY, new ResourceLocation(WoodenBucketMod.MOD_ID, name));
         }
 
     }
 
     public static class Blocks {
 
-        public static final Tag.Named<Block> BURNING = tag("burning");
-        public static final Tag.Named<Block> FREEZING = tag("freezing");
+        public static final TagKey<Block> BURNING = tag("burning");
+        public static final TagKey<Block> FREEZING = tag("freezing");
 
-        private static Tag.Named<Block> tag(@Nonnull String name) {
-            return BlockTags.bind(WoodenBucketMod.MOD_ID + ":" + name);
+        private static TagKey<Block> tag(@Nonnull String name) {
+            return TagKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation(WoodenBucketMod.MOD_ID, name));
         }
 
     }
