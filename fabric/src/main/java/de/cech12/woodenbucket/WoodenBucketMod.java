3a50accb4e71916383d42c09d009a98a3ec62d69
cech12 <cech12@gmail.com>
2026-04-24 21:34:17 +0200

    Updated to Minecraft 1.21.11 (Fabric 0.141.3+1.21.11, NeoForge 21.11.42) - Updated BucketLib to 1.21.11-4.7.0.3 - Updated Cloth Config support (21.11.153) (Fabric/Quilt) - Updated ModMenu support (17.0.0) (Fabric/Quilt)

--- fabric/src/main/java/de/cech12/woodenbucket/WoodenBucketMod.java
+++ fabric/src/main/java/de/cech12/woodenbucket/WoodenBucketMod.java
@@ -7,14 +7,14 @@ import de.cech12.woodenbucket.platform.Services;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 
 @SuppressWarnings("unused")
 public class WoodenBucketMod implements ModInitializer {
 
-    public static final ResourceLocation WOODEN_BUCKET_LOCATION = Constants.id(Constants.WOODEN_BUCKET_NAME);
+    public static final Identifier WOODEN_BUCKET_LOCATION = Constants.id(Constants.WOODEN_BUCKET_NAME);
 
     public static final Item WOODEN_BUCKET = Registry.register(BuiltInRegistries.ITEM, WOODEN_BUCKET_LOCATION, new UniversalBucketItem(
             ResourceKey.create(BuiltInRegistries.ITEM.key(), WOODEN_BUCKET_LOCATION),
