6bb06b1bc93a3167c81d4bcb1fee9b30e9d60215
cech12 <cech12@gmail.com>
2026-05-07 19:55:24 +0200

    - added possibility to use empty wooden buckets as burnables in furnaces #35 - updated to BucketLib 1.21.11-4.7.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,13 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [this versioning scheme](https://gist.github.com/cech12/69319028e88c50349a6b044000a6607b).
 
+## [1.21.11-4.7.1.0] - 2026-05-07
+### Changed
+- updated to BucketLib 1.21.11-4.7.1.0
+
+### Added
+- added possibility to use empty wooden buckets as burnables in furnaces (thanks to bixqa for the idea) #35
+
 ## [1.21.11-4.7.0.0] - 2026-04-24
 ### Changed
 - Updated to Minecraft 1.21.11 (Fabric 0.141.3+1.21.11, NeoForge 21.11.42)
--- neoforge/src/main/java/de/cech12/woodenbucket/WoodenBucketMod.java
+++ neoforge/src/main/java/de/cech12/woodenbucket/WoodenBucketMod.java
@@ -1,9 +1,7 @@
 package de.cech12.woodenbucket;
 
 import de.cech12.bucketlib.api.BucketLibApi;
-import de.cech12.bucketlib.api.item.UniversalBucketItem;
-import de.cech12.woodenbucket.init.ModTags;
-import de.cech12.woodenbucket.platform.Services;
+import de.cech12.woodenbucket.item.WoodenBucketItem;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Item;
@@ -19,20 +17,8 @@ public class WoodenBucketMod {
 
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Constants.MOD_ID);
 
-    public static final DeferredItem<Item> WOODEN_BUCKET = ITEMS.register(Constants.WOODEN_BUCKET_NAME, () -> new UniversalBucketItem(
-            ResourceKey.create(BuiltInRegistries.ITEM.key(), Constants.id(Constants.WOODEN_BUCKET_NAME)),
-            new UniversalBucketItem.Properties()
-                    .durability(Services.CONFIG::getDurability)
-                    .upperCrackingTemperature(Services.CONFIG::getBreakTemperature)
-                    .burningTemperature(Services.CONFIG::getBurningTemperature)
-                    .burningFluids(ModTags.Fluids.BURNING)
-                    .burningBlocks(ModTags.Blocks.BURNING)
-                    .freezingTemperature(Services.CONFIG::getFreezingTemperature)
-                    .freezingFluids(ModTags.Fluids.FREEZING)
-                    .freezingBlocks(ModTags.Blocks.FREEZING)
-                    .crackingFluids(ModTags.Fluids.CRACKING)
-                    .milking(Services.CONFIG::isMilkingEnabled)
-                    .entityObtaining(Services.CONFIG::isFishObtainingEnabled)
+    public static final DeferredItem<Item> WOODEN_BUCKET = ITEMS.register(Constants.WOODEN_BUCKET_NAME, () -> new WoodenBucketItem(
+            ResourceKey.create(BuiltInRegistries.ITEM.key(), Constants.id(Constants.WOODEN_BUCKET_NAME))
     ));
 
     public WoodenBucketMod(IEventBus modEventBus) {
