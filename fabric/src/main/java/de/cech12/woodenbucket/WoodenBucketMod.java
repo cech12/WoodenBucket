fe7a6f5cb9d159db0fde06fb1934b7e3621e291e
cech12 <cech12@gmail.com>
2026-05-07 19:55:24 +0200

    - added possibility to use empty wooden buckets as burnables in furnaces #35 - updated to BucketLib 1.21.5-4.4.0.3

--- fabric/src/main/java/de/cech12/woodenbucket/WoodenBucketMod.java
+++ fabric/src/main/java/de/cech12/woodenbucket/WoodenBucketMod.java
@@ -1,9 +1,7 @@
 package de.cech12.woodenbucket;
 
 import de.cech12.bucketlib.api.BucketLibApi;
-import de.cech12.bucketlib.api.item.UniversalBucketItem;
-import de.cech12.woodenbucket.init.ModTags;
-import de.cech12.woodenbucket.platform.Services;
+import de.cech12.woodenbucket.item.WoodenBucketItem;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -16,20 +14,8 @@ public class WoodenBucketMod implements ModInitializer {
 
     public static final ResourceLocation WOODEN_BUCKET_LOCATION = Constants.id(Constants.WOODEN_BUCKET_NAME);
 
-    public static final Item WOODEN_BUCKET = Registry.register(BuiltInRegistries.ITEM, WOODEN_BUCKET_LOCATION, new UniversalBucketItem(
-            ResourceKey.create(BuiltInRegistries.ITEM.key(), WOODEN_BUCKET_LOCATION),
-            new UniversalBucketItem.Properties()
-                    .durability(Services.CONFIG::getDurability)
-                    .upperCrackingTemperature(Services.CONFIG::getBreakTemperature)
-                    .burningTemperature(Services.CONFIG::getBurningTemperature)
-                    .burningFluids(ModTags.Fluids.BURNING)
-                    .burningBlocks(ModTags.Blocks.BURNING)
-                    .freezingTemperature(Services.CONFIG::getFreezingTemperature)
-                    .freezingFluids(ModTags.Fluids.FREEZING)
-                    .freezingBlocks(ModTags.Blocks.FREEZING)
-                    .crackingFluids(ModTags.Fluids.CRACKING)
-                    .milking(Services.CONFIG::isMilkingEnabled)
-                    .entityObtaining(Services.CONFIG::isFishObtainingEnabled)
+    public static final Item WOODEN_BUCKET = Registry.register(BuiltInRegistries.ITEM, WOODEN_BUCKET_LOCATION, new WoodenBucketItem(
+            ResourceKey.create(BuiltInRegistries.ITEM.key(), WOODEN_BUCKET_LOCATION)
     ));
 
     @Override
